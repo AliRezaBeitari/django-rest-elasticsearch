db46635ef40664500ce87a22aa04b2f63e9e197a
AliReza Beitari <beitari.alireza@gmail.com>
2019-12-18 00:03:02 +0330

    ProductDocumentSerializer added

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+
 from .models import Product
+from .documents import ProductDocument
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = (
+            'id',
+            'title',
+            'summary',
+            'description',
+            'created_at',
+        )
+
+
+class ProductDocumentSerializer(DocumentSerializer):
+    class Meta:
+        # Specify the correspondent document class
+        document = ProductDocument
+
+        # List the serializer fields. Note, that the order of the fields
+        # is preserved in the ViewSet.
         fields = (
             'id',
             'title',
