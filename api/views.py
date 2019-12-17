d9e6c5cda0b6fc52eaa09739b0e8b61cfe922aad
AliReza Beitari <beitari.alireza@gmail.com>
2019-12-18 00:07:51 +0330

    ProductDocumentViewSet added

--- api/views.py
+++ api/views.py
@@ -1,9 +1,56 @@
 from rest_framework import viewsets
 
-from .serializers import ProductSerializer
+from django_elasticsearch_dsl_drf.viewsets import BaseDocumentViewSet
+from django_elasticsearch_dsl_drf.pagination import PageNumberPagination
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    IdsFilterBackend,
+    OrderingFilterBackend,
+    DefaultOrderingFilterBackend,
+    SearchFilterBackend,
+)
+
+from .serializers import ProductSerializer, ProductDocumentSerializer
 from .models import Product
+from .documents import ProductDocument
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all().order_by('-created_at')
     serializer_class = ProductSerializer
+
+
+class ProductDocumentViewSet(BaseDocumentViewSet):
+    document = ProductDocument
+    serializer_class = ProductDocumentSerializer
+    pagination_class = PageNumberPagination
+    lookup_field = 'id'
+
+    filter_backends = (
+        FilteringFilterBackend,
+        IdsFilterBackend,
+        OrderingFilterBackend,
+        DefaultOrderingFilterBackend,
+        SearchFilterBackend,
+    )
+
+    # Define search fields
+    search_fields = (
+        'title',
+        'summary',
+    )
+
+    # Define filter fields
+    filter_fields = {
+        'title': 'title.raw',
+        'summary': 'summary.raw',
+    }
+
+    # Define ordering fields
+    ordering_fields = {
+        'id': 'id',
+        'title': 'title.raw',
+    }
+
+    # Specify default ordering
+    ordering = ('id', 'title',)
