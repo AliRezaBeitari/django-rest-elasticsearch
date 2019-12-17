99595baf1c4913cd1825058135eb5301c1529317
AliReza Beitari <beitari.alireza@gmail.com>
2019-12-18 00:11:25 +0330

    new url for ProductDocumentViewSet

--- Django_REST_ElasticSearch/urls.py
+++ Django_REST_ElasticSearch/urls.py
@@ -21,7 +21,7 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'products', views.ProductViewSet, basename='product')
+router.register(r'products', views.ProductDocumentViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
